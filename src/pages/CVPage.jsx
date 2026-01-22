0eb7d650dbd0b2bf918ae5c914ebca470b12f79c
donjunior01 <juniorassobjio@.com>
2026-01-22 10:56:06 +0100

    Remove unused useTheme import

--- src/pages/CVPage.jsx
+++ src/pages/CVPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useCVContext } from '../context/CVContext';
-import { useTheme } from '../context/ThemeContext';
 import { generateAndDownloadCV, generateCVDataURL } from '../utils/pdfGenerator';
 import { sendCVByEmail, openEmailClient } from '../utils/emailService';
 import { cvData } from '../data/cvData';
